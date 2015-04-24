bc9f31e06b5a0457f433f7f2f866ea4c93641b53
Adarsh Kumar <adarshdch@gmail.com>
2015-04-24 23:20:14 +0530

    update

---
+++ SAB/SAB.DAL/EntLib/DbExtensions.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Data;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SAB.DAL.EntLib
+{
+	public static class DbExtensions
+	{
+		public static XElement XmlElement(this DataSet theDataSet)
+		{
+			var aDataSetXml = new StringBuilder();
+
+			aDataSetXml.Append("<DataSet>");
+			foreach (DataTable theTable in theDataSet.Tables)
+			{
+				aDataSetXml.Append(theTable.XmlElement());
+				aDataSetXml.Append("</Table>");
+			}
+			aDataSetXml.Append("</DataSet>");
+
+			return XElement.Parse(aDataSetXml.ToString());
+		}
+
+		public static XElement XmlElement(this DataTable theDataTable)
+		{
+			var aDataTableXml = new StringBuilder();
+
+			aDataTableXml.Append("<Table>");
+			foreach (DataRow theRow in theDataTable.Rows)
+			{
+				aDataTableXml.Append("<Row>");
+				foreach (var theField in theRow.ItemArray)
+				{
+					aDataTableXml.Append("<Col>");
+					aDataTableXml.Append(theField);
+					aDataTableXml.Append("</Col>");
+				}
+				aDataTableXml.Append("</Row>");
+			}
+			aDataTableXml.Append("</Table>");
+
+			return XElement.Parse(aDataTableXml.ToString());
+		}
+	}
+}
---
+++ SAB/SAB.DAL/EntLib/DbFactory.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+
+namespace SAB.DAL.EntLib
+{
+	public class DbFactory
+	{
+		private static readonly DatabaseProviderFactory DbProviderFactory;
+
+		static DbFactory()
+		{
+			DbProviderFactory = new DatabaseProviderFactory();
+		}
+
+		public static Database GetDatabase(string theConnnectionStringName)
+		{
+			return DbProviderFactory.Create(theConnnectionStringName);
+		}
+
+
+		public static Database GetOrgDatabase(string theOrgCode)
+		{
+			return DbProviderFactory.Create(string.Format("{0}Db", theOrgCode));
+		}
+	}
+}
